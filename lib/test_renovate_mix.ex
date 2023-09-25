094a6194107fe06b554949e00100c6f72997b20b
Thiago Presa <thiago.presa@remote.com>
2023-09-25 11:08:40 -0300

    Minimal mix app

---
+++ lib/test_renovate_mix.ex
@@ -0,0 +1,18 @@
+defmodule TestRenovateMix do
+  @moduledoc """
+  Documentation for `TestRenovateMix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestRenovateMix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_renovate_mix_test.exs
@@ -0,0 +1,8 @@
+defmodule TestRenovateMixTest do
+  use ExUnit.Case
+  doctest TestRenovateMix
+
+  test "greets the world" do
+    assert TestRenovateMix.hello() == :world
+  end
+end
